e28f30b69c0a89778f3c060bab1e3433f9b614a0
Ivo Georgiev <ivo@adex.network>
2018-12-15 23:00:50 +0200

    outpace module: retab again

--- runtime/src/adex_outpace/channel.rs
+++ runtime/src/adex_outpace/channel.rs
@@ -1,12 +1,12 @@
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize, Debug))]
 #[derive(Encode, Decode, Clone, PartialEq, Eq)]
 pub struct Channel<AccountId, Balance> {
-    pub creator: AccountId,
-    pub deposit: Balance,
-    // @TODO should valid_until be some sort of substrate-specific lifetime value?
-    pub valid_until: u64,
-    pub validators: Vec<AccountId>,
-    pub spec: Vec<u8>,
+	pub creator: AccountId,
+	pub deposit: Balance,
+	// @TODO should valid_until be some sort of substrate-specific lifetime value?
+	pub valid_until: u64,
+	pub validators: Vec<AccountId>,
+	pub spec: Vec<u8>,
 }
 
 #[derive(Decode, Encode, PartialEq)]
--- runtime/src/adex_outpace/mod.rs
+++ runtime/src/adex_outpace/mod.rs
@@ -9,48 +9,45 @@ use self::channel::{Channel, ChannelState};
 pub trait Trait: balances::Trait {}
 
 decl_module! {
-    pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-        fn channel_start(origin, channel: Channel<T::AccountId, T::Balance>) -> Result {
-            let sender = ensure_signed(origin)?;
-            ensure!(sender == channel.creator, "the sender must be channel.creator");
-            let channel_hash = T::Hashing::hash_of(&channel);
-            ensure!(!<State<T>>::exists(&channel_hash), "channel already exists");
-            // @TODO: is_valid
-            <balances::Module<T>>::decrease_free_balance(&channel.creator, channel.deposit)?;
-            <State<T>>::insert(&channel_hash, ChannelState::Active);
-            Ok(())
-        }
+	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+		fn channel_start(origin, channel: Channel<T::AccountId, T::Balance>) -> Result {
+			ensure!(ensure_signed(origin)? == channel.creator, "the sender must be channel.creator");
+			let channel_hash = T::Hashing::hash_of(&channel);
+			ensure!(!<State<T>>::exists(&channel_hash), "channel already exists");
+			// @TODO: is_valid
+			<State<T>>::insert(&channel_hash, ChannelState::Active);
+			<balances::Module<T>>::decrease_free_balance(&channel.creator, channel.deposit)?;
+			Ok(())
+		}
 
-        fn channel_withdraw_expired(origin, channel: Channel<T::AccountId, T::Balance>) -> Result {
-            let sender = ensure_signed(origin)?;
-            ensure!(sender == channel.creator, "the sender must be channel.creator");
-            let channel_hash = T::Hashing::hash_of(&channel);
-            ensure!(<State<T>>::get(&channel_hash) == Some(ChannelState::Active), "channel must be active");
-            // @TODO: check if expired
-            // @TODO: only withdraw remaining balance
-            <balances::Module<T>>::increase_free_balance_creating(&channel.creator, channel.deposit);
-            <State<T>>::insert(channel_hash, ChannelState::Expired);
-            Ok(())
-        }
+		fn channel_withdraw_expired(origin, channel: Channel<T::AccountId, T::Balance>) -> Result {
+			ensure!(ensure_signed(origin)? == channel.creator, "the sender must be channel.creator");
+			let channel_hash = T::Hashing::hash_of(&channel);
+			ensure!(<State<T>>::get(&channel_hash) == Some(ChannelState::Active), "channel must be active");
+			// @TODO: check if expired
+			// @TODO: only withdraw remaining balance
+			<State<T>>::insert(channel_hash, ChannelState::Expired);
+			<balances::Module<T>>::increase_free_balance_creating(&channel.creator, channel.deposit);
+			Ok(())
+		}
 
-        fn channel_withdraw(origin, channel: Channel<T::AccountId, T::Balance>) -> Result {
-            let sender = ensure_signed(origin)?;
-            ensure!(sender == channel.creator, "the sender must be channel.creator");
-            let channel_hash = T::Hashing::hash_of(&channel);
-            ensure!(<State<T>>::get(&channel_hash) == Some(ChannelState::Active), "channel must be active");
-            // @TODO: check state
-            // @TODO check balance leaf and etc.
-            Ok(())
-        }
-    }
+		fn channel_withdraw(origin, channel: Channel<T::AccountId, T::Balance>) -> Result {
+			ensure!(ensure_signed(origin)? == channel.creator, "the sender must be channel.creator");
+			let channel_hash = T::Hashing::hash_of(&channel);
+			ensure!(<State<T>>::get(&channel_hash) == Some(ChannelState::Active), "channel must be active");
+			// @TODO: check state
+			// @TODO check balance leaf and etc.
+			Ok(())
+		}
+	}
 }
 
 decl_storage! {
-    trait Store for Module<T: Trait> as AdExOUTPACE {
-        pub State get(state): map T::Hash => Option<ChannelState>;
-        pub Withdrawn get(withdrawn): map T::Hash => T::Balance;
-        pub WithdrawnPerUser get(withdrawn_per_user): map (T::Hash, T::AccountId) => T::Balance;
-    }
+	trait Store for Module<T: Trait> as AdExOUTPACE {
+		pub State get(state): map T::Hash => Option<ChannelState>;
+		pub Withdrawn get(withdrawn): map T::Hash => T::Balance;
+		pub WithdrawnPerUser get(withdrawn_per_user): map (T::Hash, T::AccountId) => T::Balance;
+	}
 }
 
 impl<T: Trait> Module<T> {}
