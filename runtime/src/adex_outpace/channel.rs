cfefe84618816befe9058a7783b5617afca6c0b1
Ivo Georgiev <ivo@adex.network>
2018-12-15 17:37:38 +0200

    Split channel in a separate module

---
+++ runtime/src/adex_outpace/channel.rs
@@ -0,0 +1,24 @@
+use srml_support::dispatch::Result;
+
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize, Debug))]
+#[derive(Encode, Decode, Clone, PartialEq, Eq)]
+pub struct Channel<AccountId, Balance> {
+	pub creator: AccountId,
+	pub deposit: Balance,
+	// @TODO should valid_until be some sort of substrate-specific lifetime value?
+	pub valid_until: u64,
+	pub validators: Vec<AccountId>,
+	pub spec: Vec<u8>,
+}
+impl<AccountId, Balance> Channel<AccountId, Balance>
+    where AccountId: PartialEq
+{
+    pub fn is_sender_creator(&self, sender: AccountId) -> Result {
+        match sender == self.creator {
+            true => Ok(()),
+            false => Err("not the channel creator"),
+        }
+    }
+}
+
+
--- runtime/src/adex_outpace/mod.rs
+++ runtime/src/adex_outpace/mod.rs
@@ -1,28 +1,11 @@
 use srml_support::{StorageValue, dispatch::Result};
 use {balances, system::ensure_signed};
 
-pub trait Trait: balances::Trait {}
+pub mod channel;
 
-#[cfg_attr(feature = "std", derive(Serialize, Deserialize, Debug))]
-#[derive(Encode, Decode, Clone, PartialEq, Eq)]
-pub struct Channel<AccountId, Balance> {
-	creator: AccountId,
-	deposit: Balance,
-	// @TODO should valid_until be some sort of substrate-specific lifetime value?
-	valid_until: u64,
-	validators: Vec<AccountId>,
-	spec: Vec<u8>,
-}
-impl<AccountId, Balance> Channel<AccountId, Balance>
-    where AccountId: PartialEq
-{
-    fn is_sender_creator(&self, sender: AccountId) -> Result {
-        match sender == self.creator {
-            true => Ok(()),
-            false => Err("not the channel creator"),
-        }
-    }
-}
+use self::channel::Channel;
+
+pub trait Trait: balances::Trait {}
 
 decl_module! {
 	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
@@ -36,7 +19,7 @@ decl_module! {
 		fn channel_withdraw_expired(origin, channel: Channel<T::AccountId, T::Balance>) -> Result {
 			channel.is_sender_creator(ensure_signed(origin)?)?;
 		        // @TODO check state
-			<balances::Module<T>>::increase_free_balance_creating(&sender, channel.deposit);
+			<balances::Module<T>>::increase_free_balance_creating(&channel.creator, channel.deposit);
 			Ok(())
 		}
 
